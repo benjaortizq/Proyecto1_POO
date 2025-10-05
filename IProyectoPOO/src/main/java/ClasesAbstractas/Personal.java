193afa9272eaa6b81aa8f82dd4a80b70df38fa92
Conoutlet <benjaoq@gmail.com>
2025-10-05 13:01:06 -0600

    hola

---
+++ IProyectoPOO/src/main/java/ClasesAbstractas/Personal.java
@@ -0,0 +1,8 @@
+package ClasesAbstractas;
+public class Personal {
+    String Nombre ; 
+    //falta horario, pero no se como que tipo de dato usar xd , si string o que 
+    public Personal(String nombre) {
+        this.Nombre = nombre ; 
+    };
+}
